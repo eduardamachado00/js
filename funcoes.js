db20fafb5c95c167dc76cfd75f11fb76cbd90900
Eduarda Machado <eduardamachado00@gmail.com>
2023-05-25 17:40:21 -0300

    .

--- funcoes.js
+++ funcoes.js
@@ -2,7 +2,7 @@ const soma = (num1, num2) => {
     if (num1 > 10 || num2 > 10) {
             return "somente números de 1 a 9"
     } else {
-            return num1 + num2;
+            return (num1 + num2);
     } 
 }
 console.log(soma(11,2));
