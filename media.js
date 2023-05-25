07ea53f05c323f799433016863e836e68880ac86
Eduarda Machado <eduardamachado00@gmail.com>
2023-05-25 18:20:05 -0300

    arrays

---
+++ media.js
@@ -0,0 +1,19 @@
+const alunos = ["João", "Juliana", "Ana", "Caio"];
+const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunosEMedias = [alunos, medias];
+// Crie uma função que recebe como argumento o nome de um aluno.
+// Verifique se o aluno está presente na lista e retorne a média final que se encontra no mesmo índice.
+// Caso o nome do aluno não esteja na lista, retorne uma mensagem indicando que o aluno não foi encontrado.
+
+function encontrarMedia(aluno){
+    if (listaDeAlunosEMedias[0].includes(aluno)){
+        const iAluno = listaDeAlunosEMedias[0].indexOf(aluno)
+        const media = listaDeAlunosEMedias[1][iAluno]
+        console.log(`A média de ${aluno} é ${media} `)
+    } else {
+        console.log(`Estudante não encontrado!`)
+    }
+}
+
+encontrarMedia("Caio");
