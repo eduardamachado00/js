6398c4882a4b07d3314423f632551cff29ad8daa
Eduarda Machado <eduardamachado00@gmail.com>
2023-05-25 13:29:15 -0300

    coisas novas

---
+++ templateliteral.js
@@ -0,0 +1,7 @@
+const nome= "Duda";
+const idade= 2023-1999;
+const cidade = "Porto Alegre";
+
+const apresentacao = `meu nome é ${nome} minha idade é ${idade} e minha cidade é ${cidade}`;
+
+console.log(apresentacao);
--- variables.js
+++ variables.js
@@ -1,3 +1,12 @@
-const numero = 456;
-const numeroString = "456";
-console.log(numero === numeroString);
+var respostaDeTudo = 42
+let idade = 29
+const pi = String("123")
+console.log(respostaDeTudo, idade, pi)
+{
+    var respostaDeTudo = 3.14
+    let idade = 42
+    const pi = 3.1415
+    console.log(respostaDeTudo, idade, pi)
+
+}
+console.log(respostaDeTudo, idade, pi)
